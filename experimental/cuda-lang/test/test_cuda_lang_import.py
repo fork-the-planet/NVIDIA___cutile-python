398bd57a97b07603111c5742f03d181c479fef09
Asher Mancinelli <amancinelli@nvidia.com>
2026-04-28 12:03:15 -0700

    Add cuda.lang project files
    Signed-off-by: Asher Mancinelli <amancineli@nvidia.com>
    
    Co-authored-by: Asher Mancinelli <ashermancinelli@gmail.com>

---
+++ experimental/cuda-lang/test/test_cuda_lang_import.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: Copyright (c) <2026> NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
+def test_cuda_lang_imports():
+    import cuda.lang  # noqa: F401
